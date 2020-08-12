8d2f6291c6fd4d1f19caa385103dec1243c7b36a
Iago-s <iagooliveira3845@hotmail.com>
2020-08-12 12:04:29 -0300

    Simple login page

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View, TextInput, Image, Text, TouchableOpacity } from 'react-native';
 
-import InputContainer from './components/InputContainer';
-import Input from './components/Input';
-import Botao from './components/Botao';
+import logo from './assets/logoIagoDeveloper.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <View style={styles.view}>
-        <Input 
-          defaultValue="nome"
+      <Image 
+        source={logo}
+        style={styles.img}
+      />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputText}
+          placeholder="Email"
+          placeholderTextColor="#b9c7d2"
+
+          textContentType="emailAddress"
+        /> 
+        <TextInput style={styles.inputText}
+          placeholder="Password"
+          placeholderTextColor="#b9c7d2"
+
+          textContentType="password"
         />
-        <Input />  
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
       </View>
-      <Botao />
     </View>
   );
 }
@@ -23,15 +34,63 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+
+    backgroundColor: '#B9C7D2',
+
     justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  view: {
+  img: {
+    width: 300,
+    height: 100,
+    padding: 10,
+    marginBottom: 30, 
+  },  
+
+  inputContainer: {
     width: '80%',
-    marginTop: '50%',
-    marginBottom: 10,
+    borderRadius: 4,
+    
+    borderColor: '#f5f5f5',
+  },
+
+  inputText: {
+    textAlign: 'center',
+    fontSize: 20,
     color: 'black',
+
+    height: 50,
+
+    backgroundColor: '#f5f5f5',
+
+    textDecorationLine: 'underline',
+    
+    marginBottom: 10,
+    padding: 10,
+
+    borderWidth: 1,
+    borderColor: '#f5f5f5',
+    borderRadius: 8,
+  },
+
+  button: {
+    width: '100%',
+    height: 50,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    backgroundColor: 'red',
+
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 8,
+  },
+
+  buttonText: {
+    color: '#f5f5f5', 
+    fontSize: 25,
+    fontWeight: 'bold'
   }
 });
